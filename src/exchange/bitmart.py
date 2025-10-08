b2404809c2f5b6ce211572e04b062f2c0468d367
JoeChang <joechang@JoeChangs-MacBook-Pro.local>
2025-10-08 19:42:25 +0800

    Feat: Add Bitmart Snapshot

--- src/exchange/bitmart.py
+++ src/exchange/bitmart.py
@@ -24,6 +24,7 @@ class BitmartExchangeAsync(ExchangeBase):
         self.client = await krex.bingx(
             api_key=self.portfolio["api_key"],
             api_secret=self.portfolio["api_secret"],
+            memo=self.portfolio["memo"],
             preload_product_table=False,
         )
         await self.client.__aenter__()
